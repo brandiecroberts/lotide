4d72d1aac21d982a5b2ab895e1af4db9d874ed5f
Brandie Roberts <brandiecroberts@gmail.com>
2023-01-10 21:07:25 -0800

    Comparing two arrays using booleans

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+//Implement eqArrays function
+//eqArrays function takes two arrays and returns true or false
+function eqArrays (array1, array2) {
+  if (array1.length !== array2.length){
+    return false;
+  }
+  return !array1.some(function(element, index) {
+    if (element !== array2[index]) {
+      return true;
+    }
+  })
+
+}
+  assertEqual(eqArrays([1,2,3], [1,2,3]), true);
+  assertEqual(eqArrays([1,2,3], [3,2,1]), false);
+  assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true);
+  assertEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false);
