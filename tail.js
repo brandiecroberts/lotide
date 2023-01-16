ddd42bcc67a851372764ffa025b65f31d71c85b4
Brandie Roberts <brandiecroberts@gmail.com>
2023-01-16 12:33:50 -0800

    Edited code to use module.exports

--- tail.js
+++ tail.js
@@ -1,18 +1,20 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+//   } else if (actual !== expected) {
+//     console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
 // assertEqual("Lighthouse Labs", "Bootcamp");
 // assertEqual(1, 1);
 
-function tail(array) {
+const tail = function(array) {
   const newArray = array.slice(1, array.length);
   return newArray;
-}
+};
+
 let words = ["lighthouse", "labs", "bootcamp"];
 let newArray = tail(words);
 console.log(newArray);
 
+module.exports = tail;
