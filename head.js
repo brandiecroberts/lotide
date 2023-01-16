a8f9b4d1e04a94dc860ff6d6c978433fb891ddd4
Brandie Roberts <brandiecroberts@gmail.com>
2023-01-16 12:26:39 -0800

    Updated code

--- head.js
+++ head.js
@@ -8,9 +8,12 @@ const assertEqual = function(actual, expected) {
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 
-function head(assertEqual) {
-  console.log(assertEqual)
+
+const head = function(assertEqual) {
+  console.log(assertEqual);
 };
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+module.exports = head;
