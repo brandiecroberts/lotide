3f376fb88c87a1a1a9bffd2d617054b8d80ace30
Brandie Roberts <brandiecroberts@gmail.com>
2023-01-16 14:58:01 -0800

    Refactored using mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,3 +1,24 @@
+// const tail = require('../tail');
+// // const tail = function(array) {
+// //   const newArray = array.slice(1, array.length);
+// //   return newArray;
+// // };
+
+// // let words = ["lighthouse", "labs", "bootcamp"];
+// // let newArray = tail(words);
+// // console.log(newArray);
+
+// module.exports = tail;
+
+
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-module.exports = tail;
+describe("#tail", () => {
+  it("returns [LHL] for [LHL, is, fun]", () => {
+    assert.deepEqual(tail(['LHL', 'is', 'fun']), ['is', 'fun']);
+  });
+  it("returns 'Hello' for ['Hello']", () => {
+    assert.deepEqual(tail(['Hello']), []);
+  });
+});
