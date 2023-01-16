a2b869df8356679856056dc247c4a0131f7b050f
Brandie Roberts <brandiecroberts@gmail.com>
2023-01-16 15:11:46 -0800

    Exporting functions

---
+++ index.js
@@ -0,0 +1,20 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const eqArrays = require('./eqArrays');
+
+
+
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  assertEqual: assertEqual,
+  assertArraysEqual: assertArraysEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  eqArrays: eqArrays,
+};
