27af6fb2eb3aadefce4cb0f21e7782b6a15822b1
Brandie Roberts <brandiecroberts@gmail.com>
2023-01-16 12:25:15 -0800

    Refactored using test code

---
+++ test/headTest.js
@@ -0,0 +1,10 @@
+const head = require('../head');
+
+// function head(assertEqual) {
+//   console.log(assertEqual)
+// };
+
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+module.exports = head;
