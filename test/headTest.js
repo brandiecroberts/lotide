635d03a673c75f5d14d032114ea4cc10d06cf497
Brandie Roberts <brandiecroberts@gmail.com>
2023-01-16 14:12:34 -0800

    Refactored using mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,11 +1,12 @@
-// const head = require('../head');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-// // function head(assertEqual) {
-// //   console.log(assertEqual)
-// // };
-
-// // assertEqual(head([5,6,7]), 5);
-// // assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-
-// module.exports = head;
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+});
 
